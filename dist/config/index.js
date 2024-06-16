917edff37dbc3682d8e3ca5a8e43b98db9c8dcfd
MitsuiMinoru <mitsuimino711@gamil.com>
2024-06-17 05:39:26 +1000

    a

--- dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    mongoURI: "mongodb+srv://yosukekuroki000:8KTczCoc8qGt2AxC@cluster0.fmxgor2.mongodb.net/runeforce",
-    jwtSecret: "kut10developers",
-    githubToken: "<yoursecrectaccesstoken>",
-};
-//# sourceMappingURL=index.js.map
